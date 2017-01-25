7792cb3d752588c5c592bafbf50e8126f8f4eb27
Ralph John Galindo <ralphjesy@gmail.com>
2017-01-25 10:55:22 +0800

    First Commit

---
+++ dm-crowdfunding.php
@@ -0,0 +1,26 @@
+<?php if(!defined('ABSPATH')) die('Fatal Error');
+/*
+Plugin Name: DM Crowdfunding
+Plugin URI: http://divestmedia.com
+Description: Divest Media plugin for Crowd Funding Project
+Author: ralphjesy@gmail.com
+Version: 1.0
+Author URI: http://github.com/ralphjesy12
+*/
+define( 'DM_CROWD_VERSION', '1.0' );
+define( 'DM_CROWD_MIN_WP_VERSION', '4.4' );
+define( 'DM_CROWD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'DM_CROWD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'DM_CROWD_DEBUG' , true );
+
+require_once DM_CROWD_PLUGIN_DIR . '/lib/class-crowdfunding.php';
+
+
+if(class_exists('DMCROWD'))
+{
+
+  register_activation_hook(__FILE__, array('DMCROWD', 'activate'));
+  register_deactivation_hook(__FILE__, array('DMCROWD', 'deactivate'));
+  $DMCROWD = new DMCROWD();
+
+}
---
+++ lib/class-crowdfunding.php
@@ -0,0 +1,14 @@
+<?php if(!defined('DM_CROWD_VERSION')) die('Fatal Error');
+
+if(!class_exists('DMCROWD')){
+    class DMCROWD
+    {
+        public static function activate(){
+
+        }
+        public static function deactivate(){
+        
+        }
+
+    }
+}
